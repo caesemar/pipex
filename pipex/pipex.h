7b54d78cd516eb7f98ac7adca3c4b7775e325bc6
Jose Javier Casado <47799144+caesemar@users.noreply.github.com>
2023-03-21 19:34:04 +0100

    day 2

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jocasado <jocasado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 12:49:45 by jocasado          #+#    #+#             */
-/*   Updated: 2023/03/02 18:46:39 by jocasado         ###   ########.fr       */
+/*   Updated: 2023/03/21 19:33:17 by jocasado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include <errno.h>
 # include "libft/libft.h"
 
 typedef struct s_pipex
@@ -29,8 +30,14 @@ typedef struct s_pipex
 	char	*path;
 	char	**cmd_path;
 	char	*cmd;
+	char	*cmd_fpath1;
+	char	*cmd_fpath2;
 	char	**cmd_args;
 }t_pipex;
 
+char	*comm_path(char	**envp);
+char	*cmd_path_finder(t_pipex pipex, char *cmd);
+char	*cmd_found(t_pipex pipex, char *cmd);
+char	*cmd_trim(char	*cmd);
 void	pipexf(t_pipex pipex, char *comm1, char *comm2);
 #endif
